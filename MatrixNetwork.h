a88ff3cd2d374a0b342af6c4ba7369bf46385213
Ethan <ethanstabenow@gmail.com>
2026-02-22 20:32:37 -0500

    final stuff for mod 2

--- MatrixNetwork.h
+++ MatrixNetwork.h
@@ -14,7 +14,6 @@ using namespace std;
 
 class MatrixNetwork
 {
-private:
     //data structs
     struct MatrixLayer
     {
@@ -27,14 +26,10 @@ private:
         MatrixXf weightChanges;
         VectorXf biasesChanges;
 
-        MatrixLayer(int const n, int const m)
+        MatrixLayer(int const n, int const m) :
+        n(n), m(m), weights(MatrixXf::Random(n, m) * sqrt(2.0f / static_cast<float>(n))),
+        biases(VectorXf::Zero(n))
         {
-            this->n = n;
-            this->m = m;
-
-            weights = MatrixXf::Random(n, m).matrix();
-            biases = VectorXf::Random(n, 1).matrix();
-
             weightChanges = MatrixXf(n, m).matrix();
             biasesChanges = VectorXf(n, 1).matrix();
         }
@@ -52,6 +47,8 @@ private:
     int numOfOutputs;
     int middleLayerSize;
     vector<MatrixLayer> layers;
+    static inline auto leaky = [](const float x){ return x > 0 ? 1.0f : 0.01f; };
+    static inline auto reLu = [](const float x){ return x > 0 ? x : x * 0.01f; };
 
     float summer(vector<VectorXf> &saved, VectorXf &answer, int layerI, int m, int outputI)
     {
@@ -77,7 +74,7 @@ public:
     //public data type
     struct Comparable
     {
-        VectorXf input;
+        const VectorXf input;
         VectorXf answer;
     };
 
@@ -101,116 +98,164 @@ public:
         layers.emplace_back(numOfOutputs, middleLayerSize);
     }
 
-    VectorXf forward(VectorXf &input) const
+    [[nodiscard]] VectorXf forward(VectorXf input) const
     {
         for (auto & layer : layers)
         {
-            input = (layer.weights * input + layer.biases).unaryExpr([](float x)
-            {
-                return max(0.0f, x);
-            });
+            input = (layer.weights * input + layer.biases).unaryExpr(reLu);
         }
 
         //softmax
-        //input = (input.array() - input.maxCoeff()).exp();
-        //input = input / input.sum()
-
-        //sigmoid
-        input = (input.array() - input.minCoeff()) / (input.maxCoeff() - input.minCoeff());
+        input = (input.array() - input.maxCoeff()).exp();
+        input = input / input.sum();
 
         return input;
     }
 
-    void train(VectorXf &input, VectorXf &answer, const float learningRate)
+    void train(vector<Comparable> &trainingObjects, const float learningRate)
     {
-        vector<VectorXf> saved;
-        saved.reserve(layers.size() + 1);
-        saved.emplace_back(input);
-
-        //getting output
-        for (auto & layer : layers)
+        for (const auto & [input, answer] : trainingObjects)
         {
-            input = (layer.weights * input + layer.biases).unaryExpr([](float x)
+            VectorXf copy = input;
+            vector<pair<VectorXf, VectorXf>> saved;
+            saved.emplace_back(copy, VectorXf::Zero(copy.size()));
+
+            //forward
+            for (auto &layer : layers)
             {
-                return max(0.0f, x);
-            });
+                VectorXf z = layer.weights * copy + layer.biases;
+                copy = z.unaryExpr(reLu);
+                saved.emplace_back(copy, z);
+            }
 
-            saved.emplace_back(input);
-        }
+            copy = (copy.array() - copy.maxCoeff()).exp();
+            copy = copy / copy.sum();
 
-        //backprop
-        for (int outputI = 0; outputI < numOfOutputs - 2; outputI++)
-        {
-            for (int layerI = 0; layerI < layers.size(); layerI++)
+            //back prop
+            for (int layerIndex = static_cast<int>(layers.size()) - 1; layerIndex >= 0; layerIndex--)
             {
-                //first matrix layer
-                if (layerI == layers.size() - 1)
+                //get a_k
+                if (layerIndex == static_cast<int>(layers.size()) - 1)
                 {
-                    for (int m = 0; m < layers[layerI].m; m++)
+                    for (int row{0}; row < copy.size(); row++)
                     {
-                        //weights
-                        float weightChange = saved[layerI](m) * (saved[layerI + 1][outputI] > 0 ? 1 : 0.01f) *
-                            -(answer[outputI] - saved[layerI + 1][outputI]);
-
-                        layers[layerI].weightChanges(outputI, m) += -(learningRate * weightChange);
-                    }
-
-                    //basis
-                    float basisChange = layers[layerI].biases(outputI) * (saved[layerI + 1][outputI] > 0 ? 1 : 0.01f) *
-                        -(answer[outputI] - saved[layerI + 1][outputI]);
-
-                    layers[layerI].biasesChanges(outputI) += -(learningRate * basisChange);
-
-                } else if (layerI == layers.size() - 2)
-                {
-                    for (int n = 0; n < layers[layerI].n; n++)
-                    {
-                        for (int m = 0; m < layers[layerI].m; m++)
-                        {
-                            //weights
-                            float weightChange = saved[layerI][m] * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
-                                layers[layerI + 1].weights(outputI, n) * (saved[layerI + 2][outputI] > 0 ? 1 : 0.01f) *
-                                    -(answer[outputI] - saved[layerI + 2][outputI]);
-
-                            layers[layerI].weightChanges(n, m) += -(learningRate * weightChange);
-                        }
-
-                        //basis
-                        float basisChange = layers[layerI].biases(n) * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
-                            layers[layerI + 1].weights(outputI, n) * (saved[layerI + 2][outputI] > 0 ? 1 : 0.01f) *
-                                -(answer[outputI] - saved[layerI + 2][outputI]);
-
-
-                        layers[layerI].biasesChanges(n) += -(learningRate * basisChange);
+                        // copy[row] = leaky(saved[layerIndex + 1].second[row]) * 2 * (copy[row] - answer(row));
+                        copy[row] = copy[row] - answer[row];
                     }
                 } else
                 {
-                    for (int n = 0; n < layers[layerI].n; n++)
-                    {
-                        for (int m = 0; m < layers[layerI].m; m++)
-                        {
-                            //weights
-                            float weightChange = saved[layerI][m] * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
-                                summer(saved, answer, layerI + 1, n, outputI);
-
-                            layers[layerI].weightChanges(n, m) += -(learningRate * weightChange);
-                        }
-
-                        //basis
-                        float basisChange = layers[layerI].biases(n) * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
-                            summer(saved, answer, layerI + 1, n, outputI);
-
-                        layers[layerI].biasesChanges(n) += -(learningRate * basisChange);
-                    }
+                    copy = layers[layerIndex + 1].weights.transpose() * copy;
+                    VectorXf derivative = saved[layerIndex + 1].second.unaryExpr(leaky);
+                    copy = copy.cwiseProduct(derivative);
                 }
+
+                layers[layerIndex].weights -= learningRate * (copy * saved[layerIndex].first.transpose());
+                layers[layerIndex].biases -= learningRate * copy;
+
+                //changing stuff
+                // for (int row{0}; row < layers[layerIndex].n; row++)
+                // {
+                //     //weights
+                //     for (int col{0}; col < layers[layerIndex].m; col++)
+                //     {
+                //         layers[layerIndex].weights(row, col) -= learningRate * (saved[layerIndex].first[col]
+                //             * copy[row]);
+                //     }
+                //
+                //     //biases
+                //     layers[layerIndex].biases(row) -= learningRate * copy[row];
+                // }
             }
         }
 
-        //updating
-        for (auto & layer : layers)
-        {
-            layer.update(numOfOutputs);
-        }
+    //     vector<VectorXf> saved;
+    //     saved.reserve(layers.size() + 1);
+    //     saved.emplace_back(input);
+    //
+    //     //getting output
+    //     for (auto & layer : layers)
+    //     {
+    //         input = (layer.weights * input + layer.biases).unaryExpr([](float x)
+    //         {
+    //             return max(0.0f, x);
+    //         });
+    //
+    //         saved.emplace_back(input);
+    //     }
+    //
+    //     //backprop
+    //     for (int outputI = 0; outputI < numOfOutputs - 2; outputI++)
+    //     {
+    //         for (int layerI = 0; layerI < layers.size(); layerI++)
+    //         {
+    //             //first matrix layer
+    //             if (layerI == layers.size() - 1)
+    //             {
+    //                 for (int m = 0; m < layers[layerI].m; m++)
+    //                 {
+    //                     //weights
+    //                     float weightChange = saved[layerI](m) * (saved[layerI + 1][outputI] > 0 ? 1 : 0.01f) *
+    //                         -(answer[outputI] - saved[layerI + 1][outputI]);
+    //
+    //                     layers[layerI].weightChanges(outputI, m) += -(learningRate * weightChange);
+    //                 }
+    //
+    //                 //basis
+    //                 float basisChange = layers[layerI].biases(outputI) * (saved[layerI + 1][outputI] > 0 ? 1 : 0.01f) *
+    //                     -(answer[outputI] - saved[layerI + 1][outputI]);
+    //
+    //                 layers[layerI].biasesChanges(outputI) += -(learningRate * basisChange);
+    //
+    //             } else if (layerI == layers.size() - 2)
+    //             {
+    //                 for (int n = 0; n < layers[layerI].n; n++)
+    //                 {
+    //                     for (int m = 0; m < layers[layerI].m; m++)
+    //                     {
+    //                         //weights
+    //                         float weightChange = saved[layerI][m] * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
+    //                             layers[layerI + 1].weights(outputI, n) * (saved[layerI + 2][outputI] > 0 ? 1 : 0.01f) *
+    //                                 -(answer[outputI] - saved[layerI + 2][outputI]);
+    //
+    //                         layers[layerI].weightChanges(n, m) += -(learningRate * weightChange);
+    //                     }
+    //
+    //                     //basis
+    //                     float basisChange = layers[layerI].biases(n) * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
+    //                         layers[layerI + 1].weights(outputI, n) * (saved[layerI + 2][outputI] > 0 ? 1 : 0.01f) *
+    //                             -(answer[outputI] - saved[layerI + 2][outputI]);
+    //
+    //
+    //                     layers[layerI].biasesChanges(n) += -(learningRate * basisChange);
+    //                 }
+    //             } else
+    //             {
+    //                 for (int n = 0; n < layers[layerI].n; n++)
+    //                 {
+    //                     for (int m = 0; m < layers[layerI].m; m++)
+    //                     {
+    //                         //weights
+    //                         float weightChange = saved[layerI][m] * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
+    //                             summer(saved, answer, layerI + 1, n, outputI);
+    //
+    //                         layers[layerI].weightChanges(n, m) += -(learningRate * weightChange);
+    //                     }
+    //
+    //                     //basis
+    //                     float basisChange = layers[layerI].biases(n) * (saved[layerI + 1][n] > 0 ? 1 : 0.01f) *
+    //                         summer(saved, answer, layerI + 1, n, outputI);
+    //
+    //                     layers[layerI].biasesChanges(n) += -(learningRate * basisChange);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     //updating
+    //     for (auto & layer : layers)
+    //     {
+    //         layer.update(numOfOutputs);
+    //     }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,48 +1,170 @@
 #include <iostream>
 #include <fstream>
+#include <random>
 #include "MatrixNetwork.h"
 using namespace std;
 
 //data type
-struct Mnist : public MatrixNetwork::Comparable
+struct Mnist : MatrixNetwork::Comparable
 {
-    int label;
+    const int label;
 
-    Mnist(const VectorXf &input, const int label)
+    Mnist(const VectorXf &input, const int label) :
+    Comparable(input, VectorXf::Zero(10)), label{label}
     {
-        this->label = label;
-        this->input = input;
-        answer = VectorXf(10, 1);
         answer[label] = 1;
     }
 };
 
 int main()
 {
-    //read in data
-    VectorXf test(784, 1);
-    VectorXf answer(10, 1);
+    //globals
+    vector<MatrixNetwork::Comparable> train;
+    vector<Mnist> test;
 
+    //read in data
     if (ifstream file("../MNIST_CSV/mnist_train.csv"); file)
     {
-        char comma;
-        int label;
-        file >> label;
-        answer[label] = 1;
-
-        for (int i = 0; i < 784; i++)
+        for (int i{}; i < 100; i++)
         {
-            file >> comma >> test[i];
-            test[i] /= 253;
-        }
+            char comma;
+            int label;
+            VectorXf input(784);
 
+            file >> label;
+
+            for (int j = 0; j < 784; j++)
+            {
+                file >> comma >> input[j];
+                input[j] = input[j] / 255;
+            }
+
+            //if (i == 44)
+            //{
+                train.push_back(Mnist(input, label));
+            //}
+        }
     } else
     {
         cout << "file not found";
         exit(0);
     }
 
+    if (ifstream file("../MNIST_CSV/mnist_test.csv"); file)
+    {
+        for (int i{}; i < 100; i++)
+        {
+            char comma;
+            int label;
+            VectorXf input(784);
+
+            file >> label;
+
+            for (int j = 0; j < 784; j++)
+            {
+                file >> comma >> input[j];
+                input[j] = input[j] / 255;
+            }
+
+            test.emplace_back(input, label);
+        }
+    } else
+    {
+        cout << "file not found";
+        exit(0);
+    }
+
+    //user input
+    bool check = true;
+    int layers{};
+    int middleLayerSize{};
+    float learningRate{};
+
+    cout << "Please enter the number of layers you want: " << endl;
+    while (check)
+    {
+        check = true;
+        if (!(cin >> layers))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter a valid int: " << endl;
+        } else if (!(layers > 0 && layers < 8))
+        {
+            cout << "number given should not be either negative or greater than 8: " << endl;
+        } else
+        {
+            check = false;
+        }
+    }
+
+    check = true;
+    cout << "Please enter the size of the middle layers: " << endl;
+    while (check)
+    {
+        check = true;
+        if (!(cin >> middleLayerSize))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter a valid int: " << endl;
+        } else if (!(middleLayerSize > 0 && middleLayerSize <= 100))
+        {
+            cout << "number given should not be either negative or greater than 100: " << endl;
+        } else
+        {
+            check = false;
+        }
+    }
+
+    check = true;
+    cout << "Please enter the learning rate: " << endl;
+    while (check)
+    {
+        check = true;
+        if (!(cin >> learningRate))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter a valid float: " << endl;
+        } else if (!(learningRate > 0 && learningRate <= 0.001))
+        {
+            cout << "number given should not be either negative or greater than 0.001: " << endl;
+        } else
+        {
+            check = false;
+        }
+    }
+
     //create matrix
-    auto m = MatrixNetwork(5, 784, 10, 16);
-    m.train(test, answer, 0.01);
+    auto m = MatrixNetwork(layers, 784, 10, middleLayerSize);
+    for (int epoch = 0; epoch < 40; epoch++)
+    {
+        m.train(train, learningRate);
+    }
+
+    //output
+    int numCorrect{0};
+    for (auto &image : test)
+    {
+        float max{0};
+        int maxIndex{0};
+        auto output = m.forward(image.input);
+        for (int row = 0; row < 10; row++)
+        {
+            cout << output(row) << " ";
+            if (output(row) > max)
+            {
+                max = output(row);
+                maxIndex = row;
+            }
+        }
+        if (maxIndex == image.label)
+        {
+            numCorrect++;
+        }
+        cout << image.label << endl;
+    }
+
+    cout << endl << numCorrect << endl;
 }
